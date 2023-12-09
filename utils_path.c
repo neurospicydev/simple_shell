bbb9bc627b1ae9b3ef12ea79e09c4f6bef1837f8
Loïc Kami wsl <lkami@bogital.com>
2023-12-09 19:29:03 +0100

    Refactored utils_path.c

--- utils_path.c
+++ utils_path.c
@@ -2,6 +2,43 @@
 #include <sys/stat.h>
 
 /**
+ * is_path - Check if string is formatted as path
+ *
+ * Return: 1 if is path, 0 otherwise
+ */
+int is_path(char *test_path)
+{
+	int len = _strlen(test_path), i = 0;
+
+	/* Checks if path (contains char /)*/
+	for (i = 0; i < len; i++)
+	{
+		if (test_path[i] == '/')
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_existing_path - Check if path exist
+ *
+ * Return: 1 if is path, 0 otherwise
+ */
+int is_existing_path(char *test_path)
+{
+	struct stat st;
+
+	/* cmd path not found */
+	if (stat(test_path, &st) != 0)
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
  * get_cmd_path - Determines the path of command
  * @name: Name/path of command
  *
@@ -9,22 +46,14 @@
  */
 char *get_cmd_path(char *name)
 {
-	int len = _strlen(name), i = 0;
 	char *bin_path = "/bin/", *path;
 	int path_len;
-	struct stat st;
 
 	if (name == NULL)
 		return (NULL);
-	if (stat(name, &st) == 0)
-		return (name);
 
-	/* Checks if path (contains char /)*/
-	for (i = 0; i < len; i++)
-	{
-		if (name[i] == '/')
-			return (name);
-	}
+	if (is_existing_path(name) || is_path(name))
+		return (name);
 
 	path_len = _strlen(bin_path) + _strlen(name) + 1;
 	path = malloc((path_len) * sizeof(char));
@@ -34,6 +63,7 @@ char *get_cmd_path(char *name)
 		free(name);
 		return (NULL);
 	}
+
 	_memset(path, '\0', path_len);
 	/* is not path, then concat bin_path */
 	path = _strcat(path, bin_path);
@@ -41,11 +71,12 @@ char *get_cmd_path(char *name)
 
 	/* Free the name here */
 	free(name);
-	/* cmd path not found */
-	if (stat(path, &st) != 0)
+
+	if (!is_existing_path(path))
 	{
 		free(path);
-		return (NULL);
+		path = NULL;
 	}
+
 	return (path);
 }
