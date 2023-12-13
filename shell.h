f56819f5a70865dd8e0585b571b12ae5334f75c3
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-12-13 23:18:59 +0100

    fix merge conflict

--- shell.h
+++ shell.h
@@ -11,6 +11,32 @@
 
 extern char **environ;
 
+/**
+ * struct Node - A structure representing a node
+ * in a linked list
+ * @dir: Pointer to the address of the string
+ * containing the directory associated with the node
+ * @next: Pointer to the address of the next node
+ * in the linked list
+ */
+
+struct Node
+{
+	char *dir;
+	struct Node *next;
+};
+
+char *_getenv(char *envname);
+int _strlen(const char *s);
+int _strncmp(const char *s1, const char *s2, size_t n);
+void print_env(char *envname);
+char *_strtok(char *str, const char *delim);
+void addNode(struct Node **head, const char *dir);
+struct Node *build_linked_list(void);
+void print_list(struct Node *head);
+void free_list(struct Node *head);
+int _setenv(const char *name, const char *value, int overwrite);
+
 /* UTILS - SHELL */
 char **build_cmd_array(char *line, int *token_count);
 int handle_cmd(char *program_name, char **cmd, int *token_count);
