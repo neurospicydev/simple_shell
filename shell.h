f1be56c815e7dc37a2e99c3f0cfc537246552e47
Nkem Ike-Morris <fullsnahkdeveloper@gmail.com>
2023-12-13 15:55:27 -0800

    fix _strlen conflict

--- shell.h
+++ shell.h
@@ -31,18 +31,11 @@ struct Node
 	struct Node *next;
 };
 
-
-char *_getenv(char *envname);
-int _strncmp(const char *s1, const char *s2, size_t n);
 void print_env(char *envname);
-char *_strtok(char *str, const char *delim);
-void print_env(char *envname);
-char *_strtok(char *str, const char *delim);
 void addNode(struct Node **head, const char *dir);
 struct Node *build_linked_list(void);
 void print_list(struct Node *head);
 void free_list(struct Node *head);
-int _setenv(const char *name, const char *value, int overwrite);
 
 /* UTILS - SHELL */
 char **build_cmd_array(char *line, int *token_count);
